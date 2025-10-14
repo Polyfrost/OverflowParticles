b3b781174dae2940a10229f863ecca9a253ebe06
Deftu <me@deftu.dev>
2025-10-14 22:03:21 +0200

    Clean view works on all versions now

--- versions/1.21.1-fabric/src/main/java/org/polyfrost/overflowparticles/mixin/client/Mixin_CleanView.java
+++ versions/1.21.1-fabric/src/main/java/org/polyfrost/overflowparticles/mixin/client/Mixin_CleanView.java
@@ -3,10 +3,11 @@ package org.polyfrost.overflowparticles.mixin.client;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import dev.deftu.omnicore.api.client.OmniClient;
 import dev.deftu.omnicore.api.client.options.OmniPerspective;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.particle.EntityEffectParticleEffect;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.world.World;
 import org.polyfrost.overflowparticles.client.config.OverflowParticlesConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -15,23 +16,23 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(LivingEntity.class)
 public class Mixin_CleanView {
     @WrapWithCondition(
-        method = "tickEffects",
+        method = "tickStatusEffects",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"
+            target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"
         )
     )
     private boolean overflowparticles$cleanView(
-        Level level,
-        ParticleOptions particle,
-        double x, double y, double z,
-        double xSpeed, double ySpeed, double zSpeed
+            World level,
+            ParticleEffect particle,
+            double x, double y, double z,
+            double xSpeed, double ySpeed, double zSpeed
     ) {
         return !shouldBlock(particle);
     }
 
     @Unique
-    private boolean shouldBlock(ParticleOptions particle) {
+    private boolean shouldBlock(ParticleEffect particle) {
         if (!OverflowParticlesConfig.isCleanView() || OmniPerspective.getCurrentPerspective() != OmniPerspective.FIRST_PERSON) {
             return false;
         }
@@ -41,6 +42,7 @@ public class Mixin_CleanView {
             return false;
         }
 
-        return particle == ParticleTypes.ENTITY_EFFECT || particle == ParticleTypes.AMBIENT_ENTITY_EFFECT;
+        return particle.getType() == ParticleTypes.ENTITY_EFFECT
+            || particle instanceof EntityEffectParticleEffect;
     }
 }
