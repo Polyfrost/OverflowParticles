072dd5c588fbe222862b0aeb5e895b265df209f8
Microcontrollers <66657148+MicrocontrollersDev@users.noreply.github.com>
2026-07-31 23:12:05 -0700

    chore: replace modifyconstants (#27)

--- src/main/java/org/polyfrost/overflowparticles/mixin/client/particles/Mixin_ApplyMultiplierToArrows.java
+++ src/main/java/org/polyfrost/overflowparticles/mixin/client/particles/Mixin_ApplyMultiplierToArrows.java
@@ -5,20 +5,19 @@ import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 //?} else {
 /*import net.minecraft.world.entity.projectile.AbstractArrow;
 *///?}
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.polyfrost.overflowparticles.client.config.ParticleConfig;
 import org.polyfrost.overflowparticles.client.config.PerParticleConfigManager;
 import org.polyfrost.overflowparticles.client.particles.VanillaParticles;
 import org.polyfrost.overflowparticles.client.utils.ParticleSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
 public class Mixin_ApplyMultiplierToArrows {
-    @ModifyConstant(method = "tick", constant = @Constant(intValue = 4, ordinal = 0))
+    @ModifyExpressionValue(method = "tick", at = @At(value = "CONSTANT", args = "intValue=4", ordinal = 0))
     private int multiplier(int constant) {
         ParticleConfig config = PerParticleConfigManager.getConfigByType(VanillaParticles.CRITICAL);
         if (config.getMultiplier() == 1) {
@@ -28,7 +27,7 @@ public class Mixin_ApplyMultiplierToArrows {
         return (int) (constant * config.getMultiplier());
     }
 
-    @ModifyConstant(method = "tick", constant = @Constant(doubleValue = 4.0D))
+    @ModifyExpressionValue(method = "tick", at = @At(value = "CONSTANT", args = "doubleValue=4.0D"))
     private double multiplier1(double constant) {
         ParticleConfig config = PerParticleConfigManager.getConfigByType(VanillaParticles.CRITICAL);
         if (config.getMultiplier() == 1) {
